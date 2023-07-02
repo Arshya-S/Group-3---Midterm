813c8babb23ca170774575fc8416a991b05b6748
David <davidteng95@gmail.com>
2023-07-02 02:04:36 +0000

    create users table and fake inserts

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,16 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- INSERT INTO users (name) VALUES ('Alice');
+-- INSERT INTO users (name) VALUES ('Kira');
+
+-- fake users info
+INSERT INTO users (name, email, password) VALUES
+('John Doe', 'johndoe@example.com', 'password123'),
+('Jane Smith', 'janesmith@example.com', 'letmein456'),
+('Michael Johnson', 'michaeljohnson@example.com', 'securepass789'),
+('Emily Davis', 'emilydavis@example.com', 'password!@#'),
+('David Wilson', 'davidwilson@example.com', '12345678'),
+('Sarah Brown', 'sarahbrown@example.com', 'pass1234'),
+('Robert Taylor', 'roberttaylor@example.com', 'qwertyuiop'),
+('Jessica Thomas', 'jessicathomas@example.com', 'password321'),
+('Christopher Anderson', 'christopheranderson@example.com', 'testpass'),
+('Jennifer Martinez', 'jennifermartinez@example.com', 'passw0rd!');
