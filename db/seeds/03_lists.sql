9875072d502b9bcb3cc99d56be1af5581ff6d366
David <davidteng95@gmail.com>
2023-07-02 02:06:01 +0000

    create lists table and fake inserts

---
+++ db/seeds/03_lists.sql
@@ -0,0 +1,6 @@
+INSERT INTO lists (user_id, category_id, title, status) VALUES
+(1, 1, 'Ironman', FALSE),
+(2, 1, 'Spiderman', FALSE),
+(3, 2, 'Mcdonalds', TRUE),
+(1, 3, 'Calculus: Early Transcendentals', FALSE),
+(2, 4, 'Computer', TRUE);
