aa7e0dde78b866d3f4c0c4de4a8221635a8f45cf
David <davidteng95@gmail.com>
2023-07-03 21:02:37 +0000

    add form submission

---
+++ public/scripts/form.js
@@ -0,0 +1,63 @@
+$(() => {
+  const data = [
+    {
+      "task": "Watch spider-man"
+      }
+  ];
+
+  const renderTasks = function(tasks) {
+    const $task = $('.old-tasks');
+    $task.empty();
+
+    for (const task of tasks) {
+      const $taskInfo = createTask(task);
+      $task.prepend($taskInfo);
+    };
+  };
+
+  const createTask = (data) => {
+    const $task = $(`
+    <div class="old-task">${data.task}</div>
+    `);
+
+    $task.find('.old-task').text(data.task);
+
+    return $task;
+  };
+
+  $('.task-submission').on("submit", function(event) {
+    event.preventDefault();
+
+    if ($('#task-input').val() === "" || $('#task-input').val() === null) {
+      alert(`Please type in a task`);
+      return;
+    }
+
+    const data = $('.task-submission').serialize();
+
+    $.ajax({
+      url: "/",
+      method: 'POST',
+      data: data,
+      success: () => {
+        loadTasks();
+
+        $('#task-input').val('');
+        //$('.error').hide()
+      }
+    });
+  });
+
+  const loadTasks = () => {
+    $.ajax({
+      url: "/",
+      method: 'GET',
+      success: (data) => {
+        renderTasks(data);
+      }
+    });
+  };
+
+  loadTasks();
+  //$('.error').hide();
+});
