078d270c3ff0d79289c58d26452113a9949ad6fd
David <davidteng95@gmail.com>
2023-07-04 18:56:55 +0000

    modify values

--- db/queries/getTodo.js
+++ db/queries/getTodo.js
@@ -1,10 +1,16 @@
 const db = require('../connection');
 
-const getTodo = () => {
+const getTodo = (list) => {
   return db
-    .query(`SELECT * FROM lists`)
+    .query(`
+    INSERT INTO lists
+    (title, user_id, category_id)
+    VALUES ($1, $2, $3) RETURNING *;`, [list.title, list.user_id, list.category_id])
     .then(data => {
       return data.rows;
+    })
+    .catch(err => {
+      console.log('Query Error: ', err);
     });
 };
 
