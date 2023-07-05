a141bf10a097dce2621c5e42af7c3b7a296475f4
David <davidteng95@gmail.com>
2023-07-05 02:26:15 +0000

    regenerate form with Arysha and mentor

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,63 +1,18 @@
 $(() => {
-  // const data = [
-  //   {
-  //     "task": "Watch spider-man"
-  //     }
-  // ];
-
-  const renderTasks = function(tasks) {
-    const $task = $('#to-do-container');
-    $task.empty();
-
-    for (const task of tasks) {
-      const $taskInfo = createTask(task);
-      $task.prepend($taskInfo);
-    };
-  };
-
-  const createTask = (data) => {
-    const $task = $(`
-    <div class="old-task">${data.task}</div>
-    `);
-
-    $task.find('.old-task').text(data.task);
-
-    return $task;
-  };
-
-  $('.task-submission').on("submit", function(event) {
+  $('.task-submission').submit( (event) => {
     event.preventDefault();
-    console.log("successful input");
-    if ($('#task-input').val() === "" || $('#task-input').val() === null) {
-      alert(`Please type in a task`);
-      return;
-    }
-
-    const data = $('.task-submission').serialize();
-
     $.ajax({
       url: '/lists/1/new',
       method: 'POST',
-      data: data,
-      success: () => {
-        loadTasks();
-
-        $('#task-input').val('');
-        //$('.error').hide()
+      dataType: "json",
+      data: {title: $('#task-input').val()},
+      success: function(data) {
+        console.log('Successful AJAX request');
+        window.location.reload();
+      },
+      error: function(error) {
+        console.log('AJAX request error:', error);
       }
     });
   });
-
-  const loadTasks = () => {
-    $.ajax({
-      url: '/lists/1/new',
-      method: 'GET',
-      success: (data) => {
-        renderTasks(data);
-      }
-    });
-  };
-
-  loadTasks();
-  //$('.error').hide();
 });
